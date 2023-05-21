11253d6d499ff9ef8a6ff850311881672ffbf20c
Heba Omar <97067717+hebaomar94@users.noreply.github.com>
2023-05-22 01:20:51 +0300

    Update new.swift

--- new.swift
+++ new.swift
@@ -25,3 +25,16 @@ productOdd *= item
 }
 print("sum of even is \(totalEven)")
 print("product of odd is \(productOdd)")
+//while loop
+var guess = Int.random(in: 1...10)
+var magicNum = Int.random(in: 1...10)
+
+// Add your code here ✨
+while guess != magicNum {
+  guess = Int.random(in: 1...10)
+  magicNum = Int.random(in: 1...10)
+  print("You guessed \(guess), and the number was \(magicNum).")
+}
+
+print("You're right it was \(guess)!")
+
