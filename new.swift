6b06cd215d7f559a38f92eb348466020e6fd6aab
Heba Omar <97067717+hebaomar94@users.noreply.github.com>
2023-05-23 23:08:48 +0300

    Update new.swift

--- new.swift
+++ new.swift
@@ -166,7 +166,30 @@ var NewSet = SetA.union(SetB)
 
 //The .symmetricDifference() operation can be used to find elements that exist in one of the given sets, but not both.
 //.subtracting(). This method creates a new set of elements by removing the overlapping data of one set from another set.
+//Booleans do have a little extra functionality that can be useful. In particular, if you call toggle() on a Boolean it will flip a true value to false, and a false value to true. To try this out, try making gameOver a variable and modifying it like this:
 
+var gameOver = false
+print(gameOver) //false
+
+gameOver.toggle()
+print(gameOver) //true
+//string together
+//+
+//string interoprotion
+let nameb = "heba"
+let quoteb = "hi my name \(nameb) go to school"
+print(quoteb)
+/*
+Swift lets us create constants using let, and variables using var.
+If you don’t intend to change a value, make sure you use let so that Swift can help you avoid mistakes.
+Swift’s strings contain text, from short strings up to whole novels. They work great with emoji and any world language, and have helpful functionality such as count and uppercased().
+You create strings by using double quotes at the start and end, but if you want your string to go over several lines you need to use three double quotes at the start and end.
+Swift calls its whole numbers integers, and they can be positive or negative. They also have helpful functionality, such as isMultiple(of:).
+In Swift decimal numbers are called Double, short for double-length floating-point number. That means they can hold very large numbers if needed, but they also aren’t 100% accurate – you shouldn’t use them when 100% precision is required, such as when dealing with money.
+There are lots of built-in arithmetic operators, such as +, -, *, and /, along with the special compound assignment operators such as += that modify variables directly.
+You can represent a simple true or false state using a Boolean, which can be flipped using the ! operator or by calling toggle().
+String interpolation lets us place constants and variables into our strings in a streamlined, efficient way.
+*/
 
 
 
