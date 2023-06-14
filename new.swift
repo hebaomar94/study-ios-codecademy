ae6db6faf873aaf5e657806d7ac0c523b45d1d03
Heba Omar <97067717+hebaomar94@users.noreply.github.com>
2023-06-14 08:12:16 +0300

    Update new.swift

--- new.swift
+++ new.swift
@@ -711,10 +711,56 @@ affect the original structure from which it was copied.
 //represent the complex characteristic of real obj
 
 
+//command_line_coursera_ios_path
+/* Command
 
+Used for
 
+cd
 
+Change Directory
 
+ls
+
+List command used for showing the content of a directory.
+
+rm
+
+Remove command used for removing a file or a directory
+
+mv
+
+Used to move files or folders to another location
+
+touch
+
+Allows creating of a new empty file or to upate a timestamp on a file
+
+cp
+
+Used to make a copy of a file or foldler
+
+mkdir
+
+Make a new directory
+
+pwd
+
+Print work directory, shows the current location in the shell
+
+cat
+
+Allows reading or concatenation of a file
+
+less
+
+Displays the contents of a file one page at a time.
+
+grep
+
+Global regular expression, allows for searching contents of files or folders
+
+*/
 
 
 
